56d74a209e77fe90386d2216248f18319ec08672
Imikan10 <basseyimikan10@gmail.com>
2024-04-30 09:53:39 +0100

    Generating lorem ipsum

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+
+// import { para } from './data'
+
+// function App() {
+//   return (
+//     <>
+//       <section className='cont'>
+//         <h4>tired of boring lorem ipsum?</h4>
+//         <form>
+//           <label htmlFor='amount'>paragraphs:</label>
+//           <input
+//             type='number'
+//             id='amount'
+//             name='amount'
+//             min={1}
+//             max={8}
+//             step={1}
+//           ></input>
+//           <button className='btn'>generate</button>
+//         </form>
+
+//         <article className='text'>
+//           {para.map((graph) => {
+//             const { para1, para2, para3, para4, para5, para6, para7, para8 } =
+//               graph
+//             return (
+//               <>
+//                 <p>{para1}</p>
+//                 <p>{para2}</p>
+//                 <p>{para3}</p>
+//                 <p>{para4}</p>
+//                 <p>{para5}</p>
+//                 <p>{para6}</p>
+//                 <p>{para7}</p>
+//                 <p>{para8}</p>
+//               </>
+//             )
+//           })}
+//         </article>
+//       </section>
+//     </>
+//   )
+// }
+
+// export default App
+
+import React, { useState } from 'react'
+import { para } from './data'
 
 function App() {
+  const [numParagraphs, setNumParagraphs] = useState(1)
+  const [generatedParagraphs, setGeneratedParagraphs] = useState([])
+
+  const handleInputChange = (e) => {
+    setNumParagraphs(parseInt(e.target.value))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (numParagraphs > 0) {
+      setGeneratedParagraphs(para.slice(0, numParagraphs))
+    } else {
+      setGeneratedParagraphs([])
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section className='cont'>
+        <h4>tired of boring lorem ipsum?</h4>
+        <form>
+          <label htmlFor='amount'>paragraphs:</label>
+          <input
+            type='number'
+            id='amount'
+            name='amount'
+            min={1}
+            max={8}
+            step={1}
+            value={numParagraphs}
+            onChange={handleInputChange}
+          />
+          <button type='submit' onClick={handleSubmit} className='btn'>
+            generate
+          </button>
+        </form>
+
+        <article className='text'>
+          {generatedParagraphs.map((paragraph, index) => {
+            // Extracting the paragraph content dynamically
+            const key = `para${index + 1}`
+            return <p key={index}>{paragraph[key]}</p>
+          })}
+        </article>
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const para = [
+  {
+    para1:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. In dolor nulla qui eligendi, non cum esse dolorem possimus odio error illum. Repudiandae, ipsum similique vero nesciunt illum delectus beatae cumque?',
+    id: 1,
+  },
+  {
+    para2:
+      'consectetur adipisicing elit. Aliquid veniam eligendi illum porro. Beatae libero ex cumque laudantium eum quam explicabo corrupti repudiandae totam aspernatur pariatur quaerat ipsa, labore alias',
+    id: 2,
+  },
+  {
+    para3:
+      'Ullam necessitatibus repudiandae cum fugiat ipsam quae aliquid. Optio eaque eum consequuntur adipisci, sint pariatur incidunt deleniti at magni natus vero facere.Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    id: 3,
+  },
+  {
+    para4:
+      'Quibusdam amet dicta quae tenetur quos doloribus iusto rerum quas explicabo odit maiores, non obcaecati provident reprehenderit numquam repellat minus velit consequatur. Ipsum dolor sit amet consectetur, adipisicing elit.',
+    id: 4,
+  },
+  {
+    para5:
+      'sit amet consectetur adipisicing elit. Molestiae, nisi cumque quos laudantium delectus doloremque rerum enim harum sapiente iusto quia, eos adipisci nobis autem fugiat perferendis? Inventore, voluptates dicta?',
+    id: 5,
+  },
+  {
+    para6:
+      'Id facere nemo dicta dolor explicabo doloremque! Maiores autem quia, perferendis aut rem aspernatur veniam recusandae nam deserunt dolore accusamus quis assumenda. Blanditiis corrupti nostrum natus sunt, aliquam voluptas? Exercitationem, neque!',
+    id: 6,
+  },
+  {
+    para7:
+      'Eius, saepe praesentium consequuntur eos ab quibusdam magnam quo! Distinctio, repudiandae dolorum maiores debitis corporis ullam dicta labore excepturi, nisi numquam libero!. Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+    id: 7,
+  },
+  {
+    para8:
+      'Eveniet tempora iste voluptates adipisci quibusdam sequi cumque quis neque maiores debitis eligendi vero ex numquam at aperiam itaque repudiandae pariatur autem expedita, ea eius harum asperiores. Suscipit?',
+    id: 8,
+  },
+]
